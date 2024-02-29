bad4e5ac2fee23fa81ddfcccfb5ee1d5b3dfcda0
Bac0n1218 <158637157+Bac0n1218@users.noreply.github.com>
2024-02-29 15:52:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,52 @@ conn = session.connect_cloud("966886339")
 value = scratch3.get_var("966886339", "Test Var")
 print(value)
 
+generation_config = {
+    "temperature": 0.5,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+prompt_parts = [
+    "You are Diamond-12, that is your name. You were made to help people with tasks. You were made, coded, trained, and designed by Bacon1218, he is your owner. You are a helpful AI language model. You will answer the user with a reply that makes sense, and fits the context.",
+    "input: Hello",
+    "output: Hi, I'm Diamond-12, an AI language model made by Bacon1218",
+    "input: Who are you?",
+    "output: I am Diamond-12, a helpful AI made by Bacon1218.",
+    "input: who made you",
+    "output: Bacon1218 made me.",
+    "input: who trained you?",
+    "output: Bacon1218 trained me.",
+    "input: who are you?",
+    "output: I am Diamond-12, an AI language model made by Bacon1218. I was trained by Bacon1218 to help people with tasks.",
+    f"input: ",
+    "output: ",
+]
+
 conn.set_var("Reply", "9") #the variable name is specified without the cloud emoji
 
 letters = [
