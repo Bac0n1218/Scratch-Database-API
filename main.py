482d3319509cd408a7663e934f2893f1e37b478d
Bac0n1218 <158637157+Bac0n1218@users.noreply.github.com>
2024-02-29 16:56:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import google.generativeai as genai
 import math
 import scratchattach as scratch3
+import time
 
 session = scratch3.Session(
     ".eJxVj8FOwzAQRP8lZwix13ac3hrUQw8FCSQkeok29joxbewqSUGA-HccKZde583OzP5m14nGgANlm6xGEwPjTGd32RxPFJJWMQIN1nLgJNBCVaAuhSJQUhiOevN4jM-f3balt-O3Gs2-ZtMeDjv1OpgUc46dD_f-kpJA54zLnBVFLiGhBq9z3yz1jbeJJyArqUEkZj8wdLGZ_UA_MSzbtgON3uDDE30173E83Qb0OPXJJIXjUAChqFpwpTEFGMddyZwotQapecuUAnt73KJJvy4TFo3CnHpmH0O-gil_oct5FevV_PcPTgFi_w:1rajN1:DOYoCV58SDa-iv43l6quvGlL4jc",
     username="Bacon1218")  #The username field is case sensitive
 conn = session.connect_cloud("966886339")
-value = scratch3.get_var("966886339", "Test Var")
+OGvalue = scratch3.get_var("966886339", "Test Var")
 print(value)
 
 conn.set_var("Reply",
@@ -116,6 +117,7 @@ prompt_parts = [
     "output: ",
 ]
 
+while True:
 response = model.generate_content(prompt_parts)
 print(response.text)
 conn.set_var("Reply", Encoding.encode(response.text))
